ef608b550f46f7299fe2644b90c112363f13d12b
JackyCCC <jacky9813@hotmail.com>
2021-12-01 07:58:50 +0800

    main.py: Moved server starting sequence into main block, several minor improvement

--- src/main.py
+++ src/main.py
@@ -1,23 +1,39 @@
 #!/usr/bin/python3
+"""
+    An example script for Better HTTP Handler
+"""
 import os
+import sys
 import http
 import http.server
 import bhh
 
-HOST = os.environ["HOST"] if "HOST" in os.environ else "0.0.0.0"
-PORT = os.environ["PORT"] if "PORT" in os.environ else "8080"
-
-server = http.server.HTTPServer(
-    (HOST, int(PORT)),
-    bhh.HTTPRequestHandler
-)
-
 @bhh.handle("GET","/test/{testvar}", optional_trail_slash=False)
 def test(hdlr:bhh.HTTPRequestHandler, testvar):
+    """
+    An example function that returns what it received in the URI.
+    """
     hdlr.send_response(http.HTTPStatus.OK)
-    hdlr.send_header("Content-Type", "text/html; charset=utf-8")
+    hdlr.send_header("Content-Type", "text/plain; charset=utf-8")
     hdlr.end_headers()
-    hdlr.wfile.write((f"<!DOCTYPE html><html><body>{testvar}</body></html>").encode("utf-8"))
+    hdlr.wfile.write((f"{testvar}").encode("utf-8"))
 
-print(f"Listening on http://{HOST}:{PORT}")
-server.serve_forever()
+if __name__ == "__main__":
+    HOST = os.environ["HOST"] if "HOST" in os.environ else "0.0.0.0"
+    PORT = os.environ["PORT"] if "PORT" in os.environ else "8080"
+
+    try:
+        PORT = int(PORT)
+    except ValueError:
+        print(
+            f"Warning: Invalid port specification: {PORT}, using default port 8080",
+            file=sys.stderr
+        )
+        PORT = 8080
+
+    server = http.server.HTTPServer(
+        (HOST, PORT),
+        bhh.HTTPRequestHandler
+    )
+    print(f"Listening on http://{HOST}:{PORT}")
+    server.serve_forever()
