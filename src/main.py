f3a483caf2ec9315097cf0f314894fc4568f214c
JackyCCC <jacky9813@hotmail.com>
2021-12-01 09:06:25 +0800

    main.py: Added POST handler example

--- src/main.py
+++ src/main.py
@@ -18,6 +18,25 @@ def test(hdlr:bhh.HTTPRequestHandler, testvar):
     hdlr.end_headers()
     hdlr.wfile.write((f"{testvar}").encode("utf-8"))
 
+@bhh.handle("POST", "/echo")
+def echo(hdlr: bhh.HTTPRequestHandler):
+    """
+    Echo the request body as plain text type.
+    """
+    if "Content-Length" in hdlr.headers:
+        try:
+            content_length = int(hdlr.headers["Content-Length"])
+        except ValueError:
+            hdlr.send_error(http.HTTPStatus.BAD_REQUEST)
+            return
+        hdlr.send_response(http.HTTPStatus.OK)
+        hdlr.send_header("Content-Type", "text/plain; charset=utf-8")
+        hdlr.end_headers()
+        hdlr.wfile.write(hdlr.rfile.read(content_length))
+    else:
+        hdlr.send_error(http.HTTPStatus.BAD_REQUEST)
+    
+
 if __name__ == "__main__":
     HOST = os.environ["HOST"] if "HOST" in os.environ else "0.0.0.0"
     PORT = os.environ["PORT"] if "PORT" in os.environ else "8080"
